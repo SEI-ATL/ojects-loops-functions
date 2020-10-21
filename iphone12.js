fcfd8f58a54a70ec5af14e7f94e21f9dd3e5b16f
Edree Huss <edreeshuss@gmail.com>
2020-10-21 15:33:17 -0400

    [starwars objects] Create objects in js

---
+++ iphone12.js
@@ -0,0 +1,3 @@
+const iphone12 = {
+    
+}
---
+++ star-wars.js
@@ -0,0 +1,10 @@
+const starwars = {
+    image: "From Google",
+    length: '1:00',
+    title: 'Special Look | The Mandalorian | Disney+',
+    username: 'Star Wars',
+    views: '5.5M',
+    uploaded: "1 day ago",
+
+}
+console.log(starwars)
